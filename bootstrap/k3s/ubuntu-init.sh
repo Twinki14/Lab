7dad7c3c7532c423e63c40f6d4dd557d9e875138
Twinki <Twinki14@users.noreply.github.com>
2025-02-27 16:43:41 -0500

    helm v2

--- bootstrap/k3s/ubuntu-init.sh
+++ bootstrap/k3s/ubuntu-init.sh
@@ -9,10 +9,13 @@ systemctl enable serial-getty@ttyS0.service
 systemctl start serial-getty@ttyS0.service
 
 # Init /etc/rancher/k3s/config.yaml from source
-wget https://raw.githubusercontent.com/Twinki14/CozyLab/main/k3s/config.yaml --force-directories -O /etc/rancher/k3s/config.yaml
+wget https://raw.githubusercontent.com/Twinki14/CozyLab/main/bootstrap/k3s/config.yaml --force-directories -O /etc/rancher/k3s/config.yaml
 
 # Install k3s
 curl -sfL https://get.k3s.io | sh
+
+# Install flux cli
+curl -s https://fluxcd.io/install.sh | sudo bash
 
 # Reboot
 reboot
